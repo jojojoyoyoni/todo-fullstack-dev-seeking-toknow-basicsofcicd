4d1a8f5a632616f7855983183c4c25e635f4096f
jojojoyoyoni <yo1327jo60@gmail.com>
2026-08-18 15:49:44 +0300

    todos/urls corrected

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TodoViewSet
 
 router = DefaultRouter()
-router.register(r'', TodoViewSet)
+router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
